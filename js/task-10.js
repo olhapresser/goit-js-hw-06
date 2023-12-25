feaea822a75710c68f09a35153c4d90067a99e94
olhapresser <olha.presser@gmail.com>
2023-12-25 15:45:50 +0200

    new

--- js/task-10.js
+++ js/task-10.js
@@ -1,41 +1,49 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
 
-const inputEl = document.querySelector(`#controls > input`);
-const createBtn = document.querySelector(`[data-create]`);
-const destroyBtn = document.querySelector(`[data-destroy]`);
-const divEl = document.querySelector(`#boxes`);
+// const inputEl = document.querySelector(`#controls > input`);
+// const createBtn = document.querySelector(`[data-create]`);
+// const destroyBtn = document.querySelector(`[data-destroy]`);
+// const divEl = document.querySelector(`#boxes`);
 
-createBtn.addEventListener(`click`, createBoxes);
-destroyBtn.addEventListener(`click`, onDestroyBtnClick);
+// createBtn.addEventListener(`click`, createBoxes);
+// destroyBtn.addEventListener(`click`, onDestroyBtnClick);
 
 
-function createBoxes(amount) {
-  amount = Number(inputEl.value);
-  const divCountity = divEl.childElementCount;
-  let markup = ``;
+// function createBoxes(amount) {
+//   amount = Number(inputEl.value);
+//   const divCountity = divEl.childElementCount;
+//   let markup = ``;
 
-  if (!divCountity){
-    for (let i = 0; i < amount; i += 1){
-      markup += `<div style = "background-color: ${getRandomHexColor()}; width: ${
-        30 + i * 10 + 'px'}; height: ${30 + i * 10 + 'px'}"></div>`
-    }
-  } else {
-    for (let i = divCountity; i < amount + divCountity; i += 1){
-      markup += `<div style = "background-color: ${getRandomHexColor()}; width: ${30 + i * 10 + 'px'}; height: ${30 + i * 10 + 'px'}"></div>`
-    }
-  }
+//   if (!divCountity){
+//     for (let i = 0; i < amount; i += 1){
+//       markup += `<div style = "background-color: ${getRandomHexColor()}; width: ${
+//         30 + i * 10 + 'px'}; height: ${30 + i * 10 + 'px'}"></div>`
+//     }
+//   } else {
+//     for (let i = divCountity; i < amount + divCountity; i += 1){
+//       markup += `<div style = "background-color: ${getRandomHexColor()}; width: ${30 + i * 10 + 'px'}; height: ${30 + i * 10 + 'px'}"></div>`
+//     }
+//   }
 
-  divEl.insertAdjacentHTML('beforeend', markup);
-  inputEl.value = ``;
+//   divEl.insertAdjacentHTML('beforeend', markup);
+//   inputEl.value = ``;
 
-}
+// }
 
-function onDestroyBtnClick(evt) {
-  inputEl.value = ``;
-  divEl.innerHTML = ``;
-}
+// function onDestroyBtnClick(evt) {
+//   inputEl.value = ``;
+//   divEl.innerHTML = ``;
+// }
+
+
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = frutis[4];
